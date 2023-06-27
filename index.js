5c937eea555730577c4383dc1871f777577f90c3
Dan Stoakes <dan.stoakes8@gmail.com>
2023-06-27 21:49:24 +0100

    Add simple index file for JS

---
+++ index.js
@@ -0,0 +1,20 @@
+import { registerBlockType } from "@wordpress/blocks";
+
+import json from "./block.json";
+
+import edit from './edit';
+import save from './save';
+
+export const blockStyle = {
+	backgroundColor: "#900",
+	color: "#fff",
+	padding: "20px",
+};
+
+const { name } = json;
+
+// Register the block
+registerBlockType(name, {
+	edit: edit,
+	save, // save: save
+});
