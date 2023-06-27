c0dd3d7a25b611dcd856821119ba0aba15c10b84
Dan Stoakes <dan.stoakes8@gmail.com>
2023-06-27 21:53:56 +0100

    Add simple index file for PHP

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Image Beside Content Block
+ * Plugin URI: https://github.com/danstoakes/2023-img-content-two-col-WP-plugin
+ * Description: A simple plugin for creating an Image Beside Content block.
+ * Version: 1.0.0
+ * Author: Dan Stoakes
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+function danstoakes_img_content_two_col_register_block () 
+{
+	register_block_type( __DIR__ ); // location of block.json
+}
+
+add_action("init", "danstoakes_img_content_two_col_register_block");
